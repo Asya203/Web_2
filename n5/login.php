639f37bd8ecc6efb5fbf5a2244c113ffcc5a2454
Asya203 <112933229+Asya203@users.noreply.github.com>
2023-08-07 18:03:45 +0300

    Update login.php

--- n5/login.php
+++ n5/login.php
@@ -53,9 +53,9 @@ else {
   $pswrd=$_POST['password'];
   $uid=0;
   $error=TRUE;
-  $user = 'u52943';
-  $pass = '2352838';
-  $db1 = new PDO('mysql:host=localhost;dbname=u52943', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  $user = 'u52941';
+  $pass = '4049862';
+  $db1 = new PDO('mysql:host=localhost;dbname=u52941', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
   if(!empty($login) and !empty($pswrd)){
     try{
       $chk=$db1->prepare("SELECT * FROM userlogin WHERE login=?");
